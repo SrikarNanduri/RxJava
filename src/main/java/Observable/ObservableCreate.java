f31aba9b1101f717fc939dcf884c2dab3413c82b
Srikar <srikarnanduri628@gmail.com>
2019-03-01 20:10:29 +0530

    Initial commit

---
+++ src/main/java/Observable/ObservableCreate.java
@@ -0,0 +1,61 @@
+package Observable;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+/**
+ * Created by Srikar on Mar, 2019
+ */
+
+
+// This is an example for observable.create()
+
+    /*Create
+     This operator creates an Observable from scratch by calling observer methods programmatically.
+     An emitter is provided through which we can call the respective interface methods when needed.
+
+     Sample Implementation: The below sample creates an Observable using Observable.create() method.
+     The create() method does not have an option to pass values. So we have to create the list beforehand and perform operations on the list inside the onNext() method.
+     The below code will print each item from the list.
+     */
+
+public class ObservableCreate {
+    public static void main(String [] args) {
+        Observable<String> source = Observable.create(emitter -> {
+            try {
+                emitter.onNext("first");
+                emitter.onNext("Second");
+                emitter.onNext("third");
+                emitter.onComplete();
+            } catch (Exception e) {
+                emitter.onError(e);
+            }
+        });
+
+
+        Observer observer = new Observer() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                System.out.println("onSubscribe");
+            }
+
+            @Override
+            public void onNext(Object o) {
+                System.out.println("onNext: " + o);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                System.out.println("onError: " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("onComplete");
+            }
+        };
+
+        source.subscribe(observer);
+    }
+}
---
+++ src/main/java/Observable/ObservableJust.java
@@ -0,0 +1,53 @@
+package Observable;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+
+/**
+ * Created by Srikar on Mar, 2019
+ */
+
+
+/*Just
+This operator takes a list of arguments (maximum 10) and converts the items into Observable items.
+just() makes only 1 emission. For instance, If an array is passed as a parameter to the just() method, the array is emitted as single item instead of individual numbers.
+Note that if you pass null to just(), it will return an Observable that emits null as an item.
+
+Sample Implementation: The below sample creates an Observable using Observable.just() method.
+The below code will print the entire list in a single emission.
+
+Difference between Observable.from() and Observable.just() —  Observable.just() emits only once whereas Observable.from()emits n times i.e. the length of the array.
+*/
+
+public class ObservableJust {
+    public static void main(String [] args){
+        Observable<String> source = Observable.just("first", "second", "third");
+        Observer<String> observer = new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                System.out.println("onSubscribe");
+            }
+
+            @Override
+            public void onNext(String strings) {
+                System.out.println("onNext: " + strings);
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("Complete");
+            }
+        };
+
+        source.subscribe(observer);
+
+    }
+}
